55bcc1fc877c6d4b0a8d93ba0e8f5d2de06c27d9
Grzegorz Pietrzak <grzegorz.pietrzak321@gmail.com>
2018-04-17 22:50:45 +0200

    sync

---
+++ SurviSupplies/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurviSupplies.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "HomeController hello!";
+        }
+    }
+}
--- SurviSupplies/Startup.cs
+++ SurviSupplies/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SurviSupplies
 {
@@ -15,17 +17,26 @@ namespace SurviSupplies
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env,
+                              IConfiguration configuration,
+                              ILogger<Startup> logger
+                              )
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseWelcomePage(new WelcomePageOptions { });
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
